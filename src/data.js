cf46cd2e20a438f8c64a16c06a262ac104dd5389
tung3122002 <tung3122002@gmail.com>
2022-01-11 09:32:41 +0700

    fix data

--- src/data.js
+++ src/data.js
@@ -2,13 +2,13 @@ export const menu = ["Trang chủ", "Đào tạo", "Hoạt động sinh viên", 
 // export const product=[{""}]
 const product = [
     {
-        id: 1, title: "Sinh viên FPT Polytechnic giành Quán quân 'FPTHackathon 2018'", desc: "FPT Edu Hackathon là cuộc thi tổ chức bởi Tổ chức Giáo dục FPT (FPT Education) dành cho sinh viên Công nghệ thông tin theo mô hình Hackathon nổi tiếng của thế giới.", img: "https://picsum.photos/300/300",
+        id: 2, title: "Sinh viên FPT Polytechnic giành Quán quân 'FPTHackathon 2018'", desc: "FPT Edu Hackathon là cuộc thi tổ chức bởi Tổ chức Giáo dục FPT (FPT Education) dành cho sinh viên Công nghệ thông tin theo mô hình Hackathon nổi tiếng của thế giới.", img: "https://picsum.photos/250/180",
     },
     {
-        id: 2, title: "Sinh viên FPT Polytechnic giành Quán quân 'FPTHackathon 2018'", desc: "FPT Edu Hackathon là cuộc thi tổ chức bởi Tổ chức Giáo dục FPT (FPT Education) dành cho sinh viên Công nghệ thông tin theo mô hình Hackathon nổi tiếng của thế giới.", img: "https://picsum.photos/300/300",
+        id: 2, title: "Sinh viên FPT Polytechnic giành Quán quân 'FPTHackathon 2018'", desc: "FPT Edu Hackathon là cuộc thi tổ chức bởi Tổ chức Giáo dục FPT (FPT Education) dành cho sinh viên Công nghệ thông tin theo mô hình Hackathon nổi tiếng của thế giới.", img: "https://picsum.photos/250/180",
     },
     {
-        id: 3, title: "Sinh viên FPT Polytechnic giành Quán quân 'FPTHackathon 2018'", desc: "FPT Edu Hackathon là cuộc thi tổ chức bởi Tổ chức Giáo dục FPT (FPT Education) dành cho sinh viên Công nghệ thông tin theo mô hình Hackathon nổi tiếng của thế giới.", img: "https://picsum.photos/300/300",
+        id: 2, title: "Sinh viên FPT Polytechnic giành Quán quân 'FPTHackathon 2018'", desc: "FPT Edu Hackathon là cuộc thi tổ chức bởi Tổ chức Giáo dục FPT (FPT Education) dành cho sinh viên Công nghệ thông tin theo mô hình Hackathon nổi tiếng của thế giới.", img: "https://picsum.photos/250/180",
     },
 ];
 
